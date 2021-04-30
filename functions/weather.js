cba46f7bf5052fe0707c397e353d25815ded5f64
sandpatrol <54188229+sandpatrol1@users.noreply.github.com>
2021-04-30 21:26:08 +0200

    added new env variable for api key and deleted old key

--- functions/weather.js
+++ functions/weather.js
@@ -2,13 +2,11 @@
 const axios = require('axios');
 
 exports.handler = async function(event, context, callback) {
-	// your server-side functionality
+	const API_KEY = process.env.WEATHER_API_KEY;
 
-	// const API_KEY = 'bc014614ade2a6685e6c5b904442bd6f';
+	console.log('Weather Forecast');
 
-	console.log('hei there');
-
-	let url = `https://api.openweathermap.org/data/2.5/weather?q=copenhagen&units=metric&appid=bc014614ade2a6685e6c5b904442bd6f`;
+	let url = `https://api.openweathermap.org/data/2.5/weather?q=copenhagen&units=metric&appid=${API_KEY}`;
 	const config = {
 		url: url
 	};
