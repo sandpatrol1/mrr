fcdd8b634c33a6f3c009977afba284be3673bc0d
sandpatrol <54188229+sandpatrol1@users.noreply.github.com>
2021-05-03 22:20:20 +0200

    added search form

---
+++ components/locationSelect/LocationSelect.js
@@ -0,0 +1,12 @@
+import styles from './LocationSelect.module.css';
+
+const LocationSelect = () => {
+	return (
+		<form>
+			<input type="text" />
+			<button type="submit">Submitt</button>
+		</form>
+	);
+};
+
+export default LocationSelect;
--- components/wind/Wind.js
+++ components/wind/Wind.js
@@ -1,14 +1,30 @@
 import {motion} from 'framer-motion';
+import {useState} from 'react';
 import styles from './Wind.module.css';
 import useFetchWindData from '../../hooks/useFetchWindData';
 import Spinner from '../spinner/Spinner';
 import Error from '../error/Error';
-import {Fragment} from 'react';
+import LocationSelect from '../locationSelect/LocationSelect';
 
 const Wind = () => {
-	const {weather, isLoading, isError} = useFetchWindData();
+	const [ state, setState ] = useState('Copenhagen');
+	const [ location, setLocation ] = useState('Copenhagen');
+	const {weather, isLoading, isError} = useFetchWindData(location);
+
+	const changeHandler = (event) => {
+		setState(event.target.value);
+	};
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		setLocation(state);
+	};
 	return (
 		<div className={styles.container}>
+			<form onSubmit={handleSubmit} className={styles.searchform}>
+				<input type="text" value={state} onChange={changeHandler} />
+				<button type="submit">Search</button>
+			</form>
 			{isLoading ? (
 				<Spinner />
 			) : isError ? (
