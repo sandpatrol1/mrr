f1d662e9bc1365f7d1e9e24050f23619d7163671
sandpatrol <54188229+sandpatrol1@users.noreply.github.com>
2021-05-04 22:37:19 +0200

    added spinner and error page image

--- components/error/Error.js
+++ components/error/Error.js
@@ -1,7 +1,19 @@
+import Image from 'next/image';
 import styles from './Error.module.css';
 
 const Error = () => {
-	return <p>ERROR</p>;
+	return (
+		<div className={styles.error}>
+			<p>Could not be found - please type again</p>
+			<Image
+				className={styles.errorImage}
+				src="/typewriter-close-up.jpg"
+				alt="Typewriter"
+				width={350}
+				height={350}
+			/>
+		</div>
+	);
 };
 
 export default Error;
--- components/spinner/Spinner.js
+++ components/spinner/Spinner.js
@@ -1,7 +1,14 @@
+import {motion} from 'framer-motion';
 import styles from './Spinner.module.css';
 
 const Spinner = () => {
-	return <p className={styles.spinner}>SPINNER</p>;
+	return (
+		<motion.div
+			className={styles.spinner}
+			animate={{x: [ 0, 100, 0, -100, 0 ], rotate: 360}}
+			transition={{duration: 3, loop: Infinity, ease: 'linear'}}
+		/>
+	);
 };
 
 export default Spinner;
