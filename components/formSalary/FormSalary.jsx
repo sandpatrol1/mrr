536370fdc3015700b0a93bee5cc31f0460c95328
sandpatrol <54188229+sandpatrol1@users.noreply.github.com>
2021-06-01 23:35:47 +0200

    added labels

--- components/formSalary/FormSalary.jsx
+++ components/formSalary/FormSalary.jsx
@@ -24,7 +24,7 @@ function FormSalary() {
 			<Input name="savingsTotal" placeholder="Savings" type="number" onChange={handleChange} />
 			<Input name="salaryMth" placeholder="Monthly Salary" type="number" onChange={handleChange} />
 			<Input name="expensesMth" placeholder="Monthly Expenses" type="number" onChange={handleChange} />
-			<Input name="salaryIncrease" placeholder="Salary Increase Yearly" type="number" onChange={handleChange} />
+			<Input name="salaryIncrease" placeholder="Salary Increase Monthly" type="number" onChange={handleChange} />
 			<Input name="savingsMth" placeholder="Savings Monthly" type="number" onChange={handleChange} />
 			<Button text="Update Salary" color="Green" />
 		</form>
